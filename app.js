193e5c3095b1a070a4193e34b5d979c5ea44f93a
Santiago Soria <lsoriac@est.ups.edu.ec>
2020-06-16 17:56:34 -0500

    implementacion comando -o

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
+//importación de módulos
 const clima = require('./controlador/clima')
+const colors = require('colors/safe')
+    //configuración comandos yargs
 const argv = require("yargs").options({
     ciudad: {
         alias: 'c',
         desc: 'Nombre de la ciudad para obtener el clima',
         demand: true
+    },
+    //Creación nuevo comando 
+    opcion: {
+        alias: 'o',
+        desc: 'p--> Presión atmosférica \nh--> Humedad',
+        demand: false
     }
 }).argv;
-
-console.log(argv.ciudad);
-
+//comprobación lectura de parámetro ingresado por consola
+//console.log(argv.ciudad);
 const getInformacion = async(ciudad) => {
-    try {
-        //ojisimoooo con este awaaaaaittt
-        const temp = await clima.getClima(argv.ciudad)
-        return `El clima de ${ciudad} es de ${temp}`
-    } catch (e) {
-        return `No se pudo obtener el clima de ${ciudad}`
+        try {
+            //await importante
+            const ob = await clima.getClima(argv.ciudad)
+            if (argv.opcion == "p") {
+                let pa = (ob.presion * 100) / 101325
+                pa = pa.toFixed(2)
+                return `El clima de ${ciudad} es de ${ob.temperatura} °C y la presión atmosférica es de ${ob.presion} hPa(hectopascal ) o ${pa} atm (atmósferas) `
+            } else if (argv.opcion == "h") {
+                return `El clima de ${ciudad} es de ${ob.temperatura} °C y la humedad es de ${ob.humedad} %`
+            } else {
+                //en caso de q no se ingrese la opción -o
+                return `El clima de ${ciudad} es de ${ob.temperatura} °C`
+            }
+        } catch (e) {
+            //Se captan los errores
+            throw new Error(`No se pudo obtener el clima de ${ciudad}, verifique que esté bien escrita la ciudad`)
+        }
     }
-}
-
-getInformacion(argv.ciudad).then(console.log).catch(console.log);
-
-
+    //getInformacion(argv.ciudad).then(console.log()).catch(console.log);
+    //Se llama y se implementan colores a los mensajes
+getInformacion(argv.ciudad).then(mensaje => {
+    console.log(colors.green(mensaje))
+}).catch(err => { console.log(colors.red(err.message)); });
 /*
 clima.getClima(argv.ciudad).then(console.log).catch(console.log)*/
--- controlador/clima.js
+++ controlador/clima.js
@@ -1,12 +1,17 @@
+//importación de módulos
 const axios = require('axios');
-
+//función
 const getClima = async(ciudad) => {
     //codifica a formato de url
     const ciudadURL = encodeURI(ciudad)
         //ojo este await
     const resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${ciudadURL}&appid=04c33f8c54c1519a32690b31efacf327&units=metric`)
         //console.log(resp);
-    return resp.data.main.temp
+    return {
+        temperatura: resp.data.main.temp,
+        presion: resp.data.main.pressure,
+        humedad: resp.data.main.humidity
+    }
 }
 module.exports = {
     getClima
